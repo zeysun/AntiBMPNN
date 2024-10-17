36a4170783ba6056d15afcdb220bf3163e1a9c68
zeysun <168666793+zeysun@users.noreply.github.com>
2024-10-17 18:51:13 -0400

    Update Initialize.sh

--- Initialize.sh
+++ Initialize.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 CURRENT_DIR=$(basename "$PWD")
 if [ "$CURRENT_DIR" == "AntiBMPNN" ]; then
-    conda create -n mlfold python=3.11 numpy pandas torch peptides scikit-learn tqdm&& conda activate mlfold
+    conda create -n mlfold python=3.11 
+    conda activate mlfold
+    pip install numpy pandas torch peptides scikit-learn tqdm
     mkdir antibmpnn_model_weights
     cd antibmpnn_model_weights
-    wget https://zenodo.org/records/13387792/files/model_weights.zip
-    unzip model_weights.zip 
-    rm model_weights.zip
+    wget https://zenodo.org/records/13381914/files/model_weights.zip
+    unzip model_weights.zip && rm model_weights.zip
     cd ..
     echo "Initialization done. You can try example_scripts.sh."
 else
