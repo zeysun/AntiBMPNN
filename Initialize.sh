6ed50e1a8477055790822499b4ad8c5995c1ec6f
zeysun <168666793+zeysun@users.noreply.github.com>
2024-12-17 15:46:57 -0500

    Add files via upload

---
+++ Initialize.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+CURRENT_DIR=$(basename "$PWD")
+
+if [ "$CURRENT_DIR" == "AntiBMPNN" ]; then
+    conda env create -f environment.yaml
+    mkdir antibmpnn_model_weights
+    cd antibmpnn_model_weights
+    wget https://zenodo.org/records/13381914/files/model_weights.zip
+    unzip model_weights.zip && rm model_weights.zip
+    cd ..
+    echo "Initialization done. You can try example_scripts.sh."
+else
+    echo "Current diractory is not AntiBMPNN, please download the git repo."
+fi
