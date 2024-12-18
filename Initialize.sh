7ea513a7963bd3e9238cdbd3e991d56dbb234a05
zeysun <168666793+zeysun@users.noreply.github.com>
2024-12-18 14:38:45 -0500

    Update Initialize.sh

--- Initialize.sh
+++ Initialize.sh
@@ -5,7 +5,7 @@ if [ "$CURRENT_DIR" == "AntiBMPNN" ]; then
     conda env create -f environment.yaml
     mkdir antibmpnn_model_weights
     cd antibmpnn_model_weights
-    wget https://zenodo.org/records/13381914/files/model_weights.zip
+    wget -O model_weights.zip https://zenodo.org/records/13381914/files/model_weights.zip
     unzip model_weights.zip && rm model_weights.zip
     cd ..
     echo "Initialization done. You can try example_scripts.sh."
